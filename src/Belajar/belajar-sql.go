f6e8e9cd8248c60e906fa9886df2e3e541d14a5b
jasonjiu <43024869+jasonjiu@users.noreply.github.com>
2020-02-10 14:44:03 +0700

    sqlexec

--- src/Belajar/belajar-sql.go
+++ src/Belajar/belajar-sql.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -29,7 +30,7 @@ func sqlQuery() {
 	}
 	defer db.Close()
 
-	var age = 27
+	var age = 29
 	rows, err := db.Query("select id, name, grade from tb_student where age = ?", age)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -55,10 +56,92 @@ func sqlQuery() {
 	}
 
 	for _, each := range result {
-		fmt.Println(each.name)
+		// fmt.Println(each.name)
+		fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.id, each.name, each.grade)
 	}
+}
+
+func sqlQueryRow() {
+	var db, err = connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = studentData{}
+	var id = "E001"
+	err = db.QueryRow("select name, grade from tb_student where id = ?", id).Scan(&result.name, &result.grade)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("name: %s\ngrade: %d\n", result.name, result.grade)
+
+}
+
+func sqlPrepare() {
+	db, err := connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("select name, grade from tb_student where id = ?")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	var result1 = studentData{}
+	stmt.QueryRow("E001").Scan(&result1.name, &result1.grade)
+	fmt.Printf("name: %s\ngrade: %d\n", result1.name, result1.grade)
+
+	var result2 = studentData{}
+	stmt.QueryRow("W001").Scan(&result2.name, &result2.grade)
+	fmt.Printf("name: %s\ngrade: %d\n", result2.name, result2.grade)
+
+	var result3 = studentData{}
+	stmt.QueryRow("B001").Scan(&result3.name, &result3.grade)
+	fmt.Printf("name: %s\ngrade: %d\n", result3.name, result3.grade)
+}
+
+func sqlExec() {
+	db, err := connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("insert into tb_student values (?, ?, ?, ?)", "G001", "Galahad", 29, 2)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("insert success!")
+
+	_, err = db.Exec("update tb_student set age = ? where id = ?", 28, "G001")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("update success!")
+
+	_, err = db.Exec("delete from tb_student where id = ?", "G001")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("delete success!")
 }
 
 func main() {
 	sqlQuery()
+	sqlQueryRow()
+	sqlPrepare()
+	sqlExec()
 }
